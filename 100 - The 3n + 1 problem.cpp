de68449c494eb53f19ae1672dcad1a63cab53a49
Md. Zahid Fesabelilla <33966829+fesabelilla@users.noreply.github.com>
2019-01-25 22:38:11 -0800

    Add files via upload

---
+++ 100 - The 3n + 1 problem.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int n,m,i,j,c,s;
+    while(scanf("%d %d",&n,&m)==2)
+    {
+        s=0;
+        cout<<n<<" "<<m;
+        if(n>m)
+        swap(m,n);
+
+        for(i=n; i<=m; i++)
+        {
+            c=1;
+            j=i;
+
+            while(j > 1)
+            {
+                if(j % 2==0)
+                    j = j/2;
+                else
+                    j = (3*j)+1;
+                c++;
+            }
+            if(c>=s)
+                s=c;
+        }
+        cout<<" "<<s<<endl;
+    }
+    return 0;
+}
+
