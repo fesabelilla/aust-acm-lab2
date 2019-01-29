8bf81d3effa1df9f243799929fafe5baa37f45b3
Md. Zahid Fesabelilla <33966829+fesabelilla@users.noreply.github.com>
2019-01-29 06:30:44 -0800

    Add files via upload

---
+++ B-good set.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,t;
+    cin>>t;
+
+    for(int j = 0 ;j<t;j++){
+            cin>>n;
+    for(int i = 0 ; i<n ;i++ )
+        cout<<2*i+1<<" ";
+    cout<<endl;
+    }
+    return 0;
+}
+
+
