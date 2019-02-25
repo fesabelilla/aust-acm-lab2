a055c91381f724d07d0368b173ed0e8a4f02a037
Md. Zahid Fesabelilla <33966829+fesabelilla@users.noreply.github.com>
2019-02-25 11:53:01 +0600

    Add files via upload

---
+++ H - 2011 Dhaka Regional Easiest.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    for(int i = 1 ; i<=t ; i++ ){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a<=20 && b<=20 && c<=20){
+            cout<<"Case "<<i<<": good"<<endl;
+        }
+        else{
+            cout<<"Case "<<i<<": bad"<<endl;
+        }
+    }
+    return 0;
+}
+
